9aff958df85942d7b94c54756fde14473a8c6767
Douglas McGregor <41552151+TalentedPianist@users.noreply.github.com>
2022-11-18 14:33:33 +0000

    Initial commit

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
-	return <div>home</div>;
+	return <section>
+			<h1>Welcome</h1>
+
+			<h2>Introduction to Git</h2>
+			
+			<img src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80" className="Welcome" />
+		</section>
 };
 
 export default Home;
