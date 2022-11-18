f23aec04418998bb656835343021fbc663bc318b
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2022-11-18 11:26:52 +0000

    set up page stracture

---
+++ my-app/src/pages/BestPractice.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BestPractice = () => {
+	return <div>BestPractice</div>;
+};
+
+export default BestPractice;
---
+++ my-app/src/pages/Origin.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Origin = () => {
+	return <div>Origin</div>;
+};
+
+export default Origin;
