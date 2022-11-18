7c34297c6af1cf339a4443d705027942cb52e1d3
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2022-11-18 12:00:07 +0000

    navbar need to be fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BestPractice from './pages/BestPractice';
+import Branches from './pages/Branches';
+import Home from './pages/Home';
+import Issues from './pages/Issues';
+import Origin from './pages/Origin';
+import Repository from './pages/Repository';
+import Whatisgit from './pages/Whatisgit';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Nopage />}>
+					<Route
+						index
+						element={<Home />}
+					/>
+					<Route
+						path="bestpractise"
+						element={
+							<BestPractice />
+						}
+					/>
+					<Route
+						path="branches"
+						element={
+							<Branches />
+						}
+					/>
+					<Route
+						path="issues"
+						element={<Issues />}
+					/>
+					<Route
+						path="origin"
+						element={<Origin />}
+					/>
+					<Route
+						path="repository"
+						element={
+							<Repository />
+						}
+					/>
+					<Route
+						path="whatisgit"
+						element={
+							<Whatisgit />
+						}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
