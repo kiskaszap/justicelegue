737465262d95d89925005d978844e7a379d48e22
Aaron Mcavoy <20315188@myclyde.ac.uk>
2022-11-18 13:57:00 +0000

    updated what is git page and app css for it

--- my-app/src/pages/Whatisgit.jsx
+++ my-app/src/pages/Whatisgit.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Whatisgit = () => {
-	return <div>Whatisgit</div>;
+	return <div>
+		 
+		<h1 className='wig-heading'>What Is Git</h1>
+		<div className='wig-text-holder'>
+			<p>Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
+
+Git is easy to learn and has a tiny footprint with lightning fast performance it outclasses
+
+Scm tools like subversion cvs perforce and ClearCase with features like cheap local companies who are using git
+
+Google Microsoft twitter linden Netflix android</p>
+
+<p>Git was originally authored by Linus Torvalds in 2005 for development of the Linux kernel, with other kernel developers contributing to its initial development.[12] Since 2005, Junio Hamano has been the core maintainer. As with most other distributed version control systems, and unlike most client–server systems, every Git directory on every computer is a full-fledged repository with complete history and full version-tracking abilities, independent of network access or a central server</p>
+		</div>
+	
+	
+	</div>;
 };
 
 export default Whatisgit;
