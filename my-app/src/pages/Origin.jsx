9b7dbb6c3fd3b222ee0a43e19dba3ee359c29631
aaron <you@example.com>
2022-11-19 22:42:10 +0000

    Added background and updated some style elements

--- my-app/src/pages/Origin.jsx
+++ my-app/src/pages/Origin.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Origin = () => {
-	return <div>Origin</div>;
+	return <div>
+
+<h1 className='origin-heading'>what is meant by Origin master</h1>
+<div className='origin-text-holder'>
+			<p>Origin: The default name given to the remote repository, from where the local repository is cloned. In other words, origin is a remote location. 
+
+Master: The default name that GIT gives to the main branch when the local repository is initialized. This branch has the finalized/up-to-date code. All other branches are based from this branch. </p>
+
+<p>Origin and Master should not be confused with origin/master branch!!' 
+
+‘Origin/master’ is a remote-tracking branch present in a local repository that tracks changes made to the master branch in the remote repository. It is cached copy of what was last pulled from the origin. </p>
+		</div>
+
+
+	</div>;
 };
 
 export default Origin;
