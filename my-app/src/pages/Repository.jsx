7f353042e3055b8c8934d24c785202ad9581cb04
douglas@douglasmcgregor.co.uk <douglas@douglasmcgregor.co.uk>
2022-12-02 00:31:19 +0000

    Implementing "yet-another-react-lightbox".

--- my-app/src/pages/Repository.jsx
+++ my-app/src/pages/Repository.jsx
@@ -1,34 +1,62 @@
-import {React, useState, render} from 'react';
+import { React, useState } from 'react';
+import Lightbox from "yet-another-react-lightbox";
+import PhotoAlbum from "react-photo-album";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 import './Repository.css';
+
+
+
 
 
 const Repository = () => {
 	
-	const images = [ 
-		{id: 1, path: require("../images/git1.png"), alt: "", caption: "Lorem ipsum"}
-	];
 
-	const [lightboxDisplay, setLightBoxDisplay] = useState(false);
-	const [imageToShow, setImageToShow] = useState('');
+	const [open, setOpen ] = useState(false);
+	const [index, setIndex] = useState(-1);
+	// https://codesandbox.io/s/yet-another-react-lightbox-examples-9qvmif?file=/src/examples/PhotoGallery.js&initialpath=/plugins/captions
+	return (
+		<>
+		<Lightbox 
+			open={open}
+			close={() => setOpen(false)}
+			slides={[
+				{ src: require("../images/git1.png"),
+				  title: "Github is awesomme",
+				  description: "Lorem ipsum....",
+				},
+				{ src: require("../images/git2.png")}, 
+				{ src: require("../images/git3.png")},
+				{ src: require("../images/git3.png")},
+				{ src: require("../images/git4.png")},
+				{ src: require("../images/git5.png")},
+				{ src: require("../images/git6.png")},
+				{ src: require("../images/git7.png")},
+				{ src: require("../images/git8.png")},
+				{ src: require("../images/git9.png")},
+				{ src: require("../images/git10.png")},
+				{ src: require("../images/git11.png")},
+			]} plugins={[Captions, Thumbnails]}>
+		</Lightbox>
+		<div className='whatGit'>
 
-
-	function Gallery() { 
-		{images.map((item, i) => {
-			<img src={item.path} alt={item.alt} caption={item.caption} />
-			
-		})};
-	}
-	
-		return(
-			<div className="whatGit">
-				<h1 className="wig-heading">Repository</h1>
-				{lightboxDisplay ?
-					<Gallery></Gallery>
-				: '' }
+			<h1 className='wig-heading'>Repository</h1>
+			<div className='wig-text-hloder'>
+				<section className='Gallery'>
+					<a><img src={require("../images/git1.png")} /></a>
+				</section>
+				
 			</div>
-		)
-	
-	
+		</div>
+		</>
+	)
+
 }
+
+
+
 
 export default Repository;
