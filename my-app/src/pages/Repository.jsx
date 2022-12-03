0774c3fb81894e6c4dcb68d762287959ef8c11e8
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2022-12-03 15:46:53 +0000

    Changes 1 03/12/2022

--- my-app/src/pages/Repository.jsx
+++ my-app/src/pages/Repository.jsx
@@ -1,17 +1,21 @@
-import { React, Component, useState } from 'react';
+import { React, useState } from 'react';
 import Lightbox from '../components/Lightbox';
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Captions from "yet-another-react-lightbox/plugins/captions";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import './Repository.css';
 
 
-function Repository() {
+const Repository = () => {
+	const [open, setOpen] = useState(false);
+	const [index, setIndex] = useState(-1);
 
 	// https://codesandbox.io/s/yet-another-react-lightbox-examples-9qvmif?file=/src/examples/PhotoGallery.js&initialpath=/plugins/captions
 
 	const images = [
-		{ id: 1, src: require("../images/git1.png"), alt: "", caption: "github is awesome"},
+		{ id: 1, src: require("../images/git1.png"), alt: "", caption: "github is awesome" },
 		{ id: 2, src: require("../images/git2.png"), alt: "", caption: "" },
 		{ id: 3, src: require("../images/git3.png"), alt: "", caption: "" },
 		{ id: 4, src: require("../images/git4.png"), alt: "", caption: "" },
@@ -24,30 +28,38 @@ function Repository() {
 		{ id: 11, src: require("../images/git11.png"), alt: "", caption: "" }
 	]
 
-	function showImage(e) {
-		e.preventDefault();
-		<Lightbox setOpen={true} />
+	function showImage() {
+		setOpen(true);
 	}
-
-	return (
-		<>
-			<div className='whatGit'>
-				<h1 className='wig-heading'>Repository</h1>
-				<div className='wig-text-holder'>
-					<section className='Gallery'>
-						{images.map((image) => // Here we map the images array above into renderable html 
-							<figure>
-								<a href="#" onClick={showImage}><img src={image.src} alt={image.alt} /></a>
-								<figcaption>{image.caption}</figcaption>
-							</figure>
-						)}
-					</section>
-				</div>
-			</div>
-
-			
-		</>
-	)
 }
+return (
+	<>
+		<button onClick={() => showImage()}>Open Lightbox</button>
+
+		<Lightbox
+			open={open}
+			close={() => setOpen(false)}
+			slides={[
+				{
+					src: require("../images/git1.png"),
+					title: "Github is awesomme",
+					description: "Lorem ipsum....",
+				},
+				{ src: require("../images/git2.png") },
+				{ src: require("../images/git3.png") },
+				{ src: require("../images/git3.png") },
+				{ src: require("../images/git4.png") },
+				{ src: require("../images/git5.png") },
+				{ src: require("../images/git6.png") },
+				{ src: require("../images/git7.png") },
+				{ src: require("../images/git8.png") },
+				{ src: require("../images/git9.png") },
+				{ src: require("../images/git10.png") },
+				{ src: require("../images/git11.png") },
+			]} plugins={[Captions, Thumbnails]}>
+		</Lightbox>
+	</>
+)
+
 
 export default Repository;
